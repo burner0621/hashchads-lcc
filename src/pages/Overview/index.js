6080e7a2a1826fbb0cc265787f4e1f9aa47194e3
newstar121 <bg.newstar121@gmail.com>
2023-05-15 22:17:05 -0700

    add spinner

--- src/pages/Overview/index.js
+++ src/pages/Overview/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Col, Container, Row } from "reactstrap";
+import { Col, Container, Row, Spinner } from "reactstrap";
 import { Box } from 'rebass'
 import styled from 'styled-components'
 
@@ -94,7 +94,7 @@ const Overview = () => {
           )}
           {!below800 && (
             <GridRow>
-              <Panel style={{ height: '100%', minHeight: '300px' }} className="panel-shadow hsla-bg">
+              <Panel style={{ height: '100%', minHeight: '300px', maxHeight: '5vh', justifyContent: 'center', alignItems:'center'}} className="panel-shadow hsla-bg d-flex">
                 <div className="animate-x-slide"
                   style={{ position: 'absolute', top: '0', left: '0.2rem', height: '0.25rem', borderRadius: '0.25rem', backgroundImage: 'linear-gradient(to right,hsla(0,0%,100%,.15),#ff007a)', filter: 'blur(1px)' }}
                 />
@@ -107,9 +107,12 @@ const Overview = () => {
                 <div className="animate-x-slide"
                   style={{ position: 'absolute', bottom: '0', right: '0.125rem', height: '0.25rem', borderRadius: '0.25rem', backgroundImage: 'linear-gradient(to right,hsla(0,0%,100%,.15),#00e9b1)', filter: 'blur(1px)' }}
                 />
-                <GlobalChart display="liquidity" />
+                <span className="d-flex align-items-center">
+                  <Spinner size="lg" className="flex-shrink-0"> Loading... </Spinner>
+                </span>
+                {/* <GlobalChart display="liquidity" /> */}
               </Panel>
-              <Panel style={{ height: '100%' }} className="panel-shadow hsla-bg">
+              <Panel style={{ height: '100%', minHeight: '300px', maxHeight: '5vh' }} className="panel-shadow hsla-bg">
                 <div className="animate-x-slide"
                   style={{ position: 'absolute', top: '0', left: '0.2rem', height: '0.25rem', borderRadius: '0.25rem', backgroundImage: 'linear-gradient(to right,hsla(0,0%,100%,.15),#ff007a)', filter: 'blur(1px)' }}
                 />
