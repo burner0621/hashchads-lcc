83f6855054a7471179bbc77d1de06c07c1e6912b
sasapapovic988 <sasapapovic988@gmail.com>
2023-07-19 03:33:37 +0900

    interval value chainged

--- src/pages/Tokens/TokenPage.js
+++ src/pages/Tokens/TokenPage.js
@@ -398,7 +398,7 @@ const TokenPage = ({ address }) => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
         timeInterval.current = setInterval(async () => {
             await fetchData()
-        }, 1000);
+        }, 5000);
     }, [currentPage, rowsPerPage])
 
     const handlePageChange = (page, totalRows) => {
@@ -407,7 +407,7 @@ const TokenPage = ({ address }) => {
         if (timeInterval.current) clearInterval(timeInterval.current);
         timeInterval.current = setInterval(async () => {
             await fetchData()
-        }, 1000);
+        }, 5000);
     }
 
     const handlePerRowsChange = async (newPerPage, page) => {
